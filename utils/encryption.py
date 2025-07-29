b17f060f10a26522d59d2852090a87f1ef305d53
VegitoTy <108405056+VegitoTy@users.noreply.github.com>
2025-07-29 16:17:25 +0530

    add comment

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,5 +1,7 @@
 import bcrypt
 
+# lazy
+
 def encrypt(password):
     encrypted_password = (f"salty{password[::-1]}moresalty").encode("utf-8")
     hashedPassword = bcrypt.hashpw(encrypted_password, bcrypt.gensalt())
